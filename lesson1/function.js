d6770b66aed3d61ca157f7bcbaf16ee52b40f10b
hoangleminh2709 <leminhlaso1@gmail.com>
2023-06-19 21:12:44 +0700

    update

---
+++ lesson1/function.js
@@ -0,0 +1,7 @@
+//1.
+    const array1 = [1,2,3,4,5,6];
+    const array2 = [3,4,8,9,12];
+    const result = array.filter(array1,array2){
+        
+    }
+        
---
+++ lesson2_practice/app.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg'
+import './app.css';
+
+function App() {
+    return (
+        <div className="App">
+            <input type ="text"
+            placeholder='Enter your task here'
+            className ='input'></input>
+        </div>
+    )
+}
